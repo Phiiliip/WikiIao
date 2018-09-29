15ad65b3b950de45c75f2ced6debbaa8fc3c6de7
Phiiliip <felilotero@gmail.com>
2018-09-29 02:13:59 -0300

    Inicio proyecto en Android

---
+++ WikiIao/app/src/main/java/com/example/felipe/wikiiao/clases.java
@@ -0,0 +1,15 @@
+package com.example.felipe.wikiiao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class clases extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clases);
+    }
+
+    public void holis(){}
+}
